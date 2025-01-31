471743bc380fd18be839d6d602561bffb1b27d4f
biboletin <shady_fan@abv.bg>
2025-02-01 00:51:26 +0200

    Create route functionality. Middleware will be implemented later

---
+++ src/Core/Interfaces/RouterInterface.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Bibo\Core\Interfaces;
+
+/**
+ * BaseRouter interface
+ */
+interface RouterInterface
+{
+    /**
+     * Set GET routes
+     *
+     * @param string         $route
+     * @param array|callable $callable
+     *
+     * @return RouterInterface
+     */
+    public function get(string $route, array|callable $callable): self;
+
+    /**
+     * Set POST routes
+     *
+     * @param string         $route
+     * @param array|callable $callable
+     *
+     * @return RouterInterface
+     */
+    public function post(string $route, array|callable $callable): self;
+
+    /**
+     * Set PUT routes
+     *
+     * @param string         $route
+     * @param array|callable $callable
+     *
+     * @return RouterInterface
+     */
+    public function put(string $route, array|callable $callable): self;
+
+    /**
+     * Set DELETE routes
+     *
+     * @param string         $route
+     * @param array|callable $callable
+     *
+     * @return RouterInterface
+     */
+    public function delete(string $route, array|callable $callable): self;
+
+    /**
+     * Group routes
+     *
+     * @param string   $name
+     * @param callable $callable
+     *
+     * @return void
+     */
+    public function group(string $name, callable $callable): void;
+
+    /**
+     * Match route
+     *
+     * @param string $method
+     * @param string $uri
+     *
+     * @return array|null
+     */
+    public function match(string $method, string $uri): ?array;
+
+    /**
+     * Add middleware
+     *
+     * @param callable $middleware
+     *
+     * @return void
+     */
+    public function middleware(callable $middleware): void;
+
+    /**
+     * Find route
+     *
+     * @param string $method
+     * @param string $route
+     *
+     * @return array|null
+     */
+    public function find(string $method, string $route): ?array;
+
+    /**
+     * Set route name
+     *
+     * @param string $name
+     *
+     * @return void
+     */
+    public function name(string $name): void;
+
+    /**
+     * Dump routes
+     *
+     * @return void
+     */
+    public function dump(): void;
+}

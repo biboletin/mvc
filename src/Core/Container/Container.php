e656bd48f8858912611abdfb6e24faf38462de5b
biboletin <shady_fan@abv.bg>
2025-02-11 22:47:17 +0200

    Create Container class

---
+++ src/Core/Container/Container.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Bibo\Core\Container;
+
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Exception;
+
+/**
+ * Simple Dependency Injection (DI) container implementing PSR-11
+ */
+class Container implements ContainerInterface
+{
+    /**
+     * Services stored in the container
+     * @var array
+     */
+    private array $services = [];
+
+    /**
+     * Bind a service to the container
+     *
+     * @param string   $id
+     * @param callable $concrete
+     *
+     * @return ContainerInterface
+     */
+    public function set(string $id, callable $concrete): ContainerInterface
+    {
+        $this->services[$id] = $concrete;
+        return $this;
+    }
+
+    /**
+     * Get a service from the container
+     *
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundExceptionInterface
+     */
+    public function get(string $id): mixed
+    {
+        if (!$this->has($id)) {
+            throw new class ($id . ' not found in container') extends Exception implements NotFoundExceptionInterface {
+            };
+        }
+
+        return $this->services[$id]($this);
+    }
+
+    /**
+     * Check if a service exists in the container
+     *
+     * @param string $id
+     * @return bool
+     */
+    public function has(string $id): bool
+    {
+        return isset($this->services[$id]);
+    }
+}

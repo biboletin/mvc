269eb86f48a6f92962e5fbe3b8880dd0fb84587b
biboletin <shady_fan@abv.bg>
2025-02-20 20:26:21 +0200

    Create test route, constants file and file permissions file

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Bibo\Core\Facades\Route;
+use Bibo\Core\Response\JsonResponse;
+
+Route::get('/', function () {
+    return new JsonResponse(['message' => 'Welcome to Home']);
+});
+
+// Route::get('/', function () {
+//     echo 'Hello';
+// });
+
+// Route::get('/', function () {
+//     return 'Hello';
+// });
+
+// Route::get('/{name}', function ($name) {
+//     return 'Hello ' . $name;
+// });
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+// Route::get('/', [Controller::class, 'index']);

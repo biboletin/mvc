ab19f3f45c33e48415155b4617221d399fb764a9
biboletin <shady_fan@abv.bg>
2025-08-31 22:03:52 +0300

    Refactor exception response handler

--- src/Core/Exception/Custom/Http/ResponseExceptionHandler.php
+++ src/Core/Exception/Custom/Http/ResponseExceptionHandler.php
@@ -18,27 +18,43 @@ class ResponseExceptionHandler extends AppException
 {
     /**
      * Handle the exception
-     *
-     * @throws JsonException
      */
     public static function handle(Throwable $exception): void
     {
-        $code = (int) $exception->getCode() ?: HttpStatus::InternalServerError->value;
+        $code = $exception->getCode() ?: HttpStatus::InternalServerError->value;
         $accept = $_SERVER['HTTP_ACCEPT'] ?? '';
 
-        if (str_contains($accept, 'application/json')) {
-            $response = new JsonResponse([
-                'error' => true,
-                'message' => $exception->getMessage(),
-                'code' => $code,
-            ], $code);
-        } else {
-            // Basic HTML error template or a full render if you have a view engine
-            $html = "<h1>Error {$code}</h1><p>{$exception->getMessage()}</p>";
-            $html .= "<pre>{$exception->getTraceAsString()}</pre>";
-            $response = new HtmlResponse($html, $code);
+        try {
+            if (str_contains($accept, 'application/json')) {
+                $response = new JsonResponse([
+                    'error' => true,
+                    'message' => $exception->getMessage(),
+                    'code' => $code,
+                ], $code);
+            } else {
+                $response = new HtmlResponse(
+                    self::formatHtmlError($exception, $code),
+                    $code
+                );
+            }
+        } catch (JsonException $e) {
+            $response = new HtmlResponse(
+                self::formatHtmlError($e, HttpStatus::InternalServerError->value),
+                HttpStatus::InternalServerError->value
+            );
         }
+
         $emitter = new ResponseEmitter();
         $emitter->emit($response);
+    }
+
+    private static function formatHtmlError(Throwable $e, int $code): string
+    {
+        return sprintf(
+            '<h1>Error %d</h1><p>%s</p><pre>%s</pre>',
+            $code,
+            htmlspecialchars($e->getMessage(), ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'),
+            htmlspecialchars($e->getTraceAsString(), ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8')
+        );
     }
 }

31e3921c2b149b7083578a377de24498d166d542
biboletin <shady_fan@abv.bg>
2025-09-19 01:03:30 +0300

    Add new class to handle compression and decompression

---
+++ src/Core/Compression/Compression.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bibo\Mvc\Core\Compression;
+
+use Bibo\Mvc\Core\Interfaces\CompressionInterface;
+
+class Compression implements CompressionInterface
+{
+    /**
+     * Compress data
+     *
+     * @param $data
+     *
+     * @return mixed
+     */
+    public function compress($data): mixed
+    {
+        // TODO: Implement compress() method.
+    }
+
+    /**
+     * Uncompress data
+     *
+     * @param $data
+     *
+     * @return mixed
+     */
+    public function uncompress($data): mixed
+    {
+        // TODO: Implement uncompress() method.
+    }
+}
